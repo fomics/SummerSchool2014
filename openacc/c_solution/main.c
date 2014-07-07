7f147759b000c647fdbb387433c13aa43c9bf2d4
Dmitry Mikushin <dmitry@kernelgen.org>
2014-07-08 01:24:52 +0200

    Adding OpenACC C reference solution of mini-stencil problem that works both with PGI (14.1) and Piz Daint's Cray (current)

---
+++ openacc/c_solution/main.c
@@ -0,0 +1,271 @@
+// ******************************************
+// implicit time stepping implementation of 2D diffusion problem
+// Ben Cumming, CSCS
+// C version by Gilles Fourestey, CSCS
+// *****************************************
+
+// A small benchmark app that solves the 2D fisher equation using second-order
+// finite differences.
+
+// Syntax: ./main nx ny nt t
+
+#include <math.h>
+#include <omp.h>
+#include <openacc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "data.h"
+#include "linalg.h"
+#include "operators.h"
+#include "stats.h"
+
+// ==============================================================================
+
+// read command line arguments
+static void readcmdline(struct discretization_t* options, int argc, char* argv[])
+{
+    if (argc != 5)
+    {
+        printf("Usage: main nx ny nt t\n");
+        printf("  nx  number of gridpoints in x-direction\n");
+        printf("  ny  number of gridpoints in y-direction\n");
+        printf("  nt  number of timesteps\n");
+        printf("  t   total time\n");
+        exit(1);
+    }
+
+    // read nx
+    options->nx = atoi(argv[1]);
+    if (options->nx < 1)
+    {
+        fprintf(stderr, "nx must be positive integer\n");
+        exit(-1);
+    }
+
+    // read ny
+    options->ny = atoi(argv[2]);
+    if (options->ny < 1)
+    {
+        fprintf(stderr, "ny must be positive integer\n");
+        exit(-1);
+    }
+
+    // read nt
+    options->nt = atoi(argv[3]);
+    if (options->nt < 1)
+    {
+        fprintf(stderr, "nt must be positive integer\n");
+        exit(-1);
+    }
+    
+    // read total time
+    double t = atof(argv[4]);
+    if (t < 0)
+    {
+        fprintf(stderr, "t must be positive real value\n");
+        exit(-1);
+    }
+
+    // store the parameters
+    options->N = options->nx*options->ny;
+
+    // compute timestep size
+    options->dt = t / options->nt;
+    
+    // compute the distance between grid points
+    // assume that x dimension has length 1.0
+    options->dx = 1./(options->nx - 1);
+    
+    // set alpha, assume diffusion coefficient D is 1
+    options->alpha = (options->dx*options->dx) / (1.*options->dt);
+}
+
+// ==============================================================================
+
+int main(int argc, char* argv[])
+{
+    acc_set_device_num(0, acc_device_nvidia);
+
+    // read command line arguments
+    readcmdline(&options, argc, argv);
+    int nx = options.nx;
+    int ny = options.ny;
+    int N  = options.N;
+    int nt = options.nt;
+
+    printf("========================================================================\n");
+    printf("                      Welcome to mini-stencil!\n");
+    printf("mesh :: %d * %d, dx = %f\n", nx, ny, options.dx);
+    printf("time :: %d, time steps from 0 .. %f\n", nt, options.nt * options.dt);
+    printf("========================================================================\n");
+
+    // allocate global fields
+    x_new = (double*) malloc(sizeof(double)*nx*ny);
+    x_old = (double*) malloc(sizeof(double)*nx*ny); 
+    bndN  = (double*) malloc(sizeof(double)*nx);
+    bndS  = (double*) malloc(sizeof(double)*nx); 
+    bndE  = (double*) malloc(sizeof(double)*ny); 
+    bndW  = (double*) malloc(sizeof(double)*ny); 
+
+    double* b      = (double*) malloc(N*sizeof(double));
+    double* deltax = (double*) malloc(N*sizeof(double));
+
+    // set dirichlet boundary conditions to 0 all around
+    memset(x_old, 0, sizeof(double) * nx * ny);
+    memset(bndN, 0, sizeof(double) * nx);
+    memset(bndS, 0, sizeof(double) * nx);
+    memset(bndE, 0, sizeof(double) * ny);
+    memset(bndW, 0, sizeof(double) * ny);
+    memset(deltax, 0, sizeof(double) * N);
+
+    // set the initial condition
+    // a circle of concentration 0.1 centred at (xdim/4, ydim/4) with radius
+    // no larger than 1/8 of both xdim and ydim
+    memset(x_new, 0, sizeof(double) * nx * ny);
+    double xc = 1.0 / 4.0;
+    double yc = (ny - 1) * options.dx / 4;
+    double radius = fmin(xc, yc) / 2.0;
+    int i,j;
+    //
+    for (j = 0; j < ny; j++)
+    {
+        double y = (j - 1) * options.dx;
+        for (i = 0; i < nx; i++)
+        {
+            double x = (i - 1) * options.dx;
+            if ((x - xc) * (x - xc) + (y - yc) * (y - yc) < radius * radius)
+                //((double(*)[nx])x_new)[j][i] = 0.1;
+                x_new[i+j*nx] = 0.1;
+        }
+    }
+
+    flops_bc = 0;
+    flops_diff = 0;
+    flops_blas1 = 0;
+    verbose_output = 0;
+    iters_cg = 0;
+    iters_newton = 0;
+
+    // initialize temporary storage fields used by the cg solver
+    // I do this here so that the fields are persistent between calls
+    // to the CG solver. This is useful if we want to avoid malloc/free calls
+    // on the device for the OpenACC implementation (feel free to suggest a better
+    // method for doing this)
+    printf("INITIALIZING CG STATE\n");
+    Ap    = (double*) malloc(N*sizeof(double));
+    r     = (double*) malloc(N*sizeof(double)); 
+    p     = (double*) malloc(N*sizeof(double));
+    Fx    = (double*) malloc(N*sizeof(double));
+    Fxold = (double*) malloc(N*sizeof(double));
+    v     = (double*) malloc(N*sizeof(double));
+    xold  = (double*) malloc(N*sizeof(double));
+
+    int cg_converged = 1;
+
+    double timespent;
+
+    #pragma acc data copy(x_old[0:nx*ny], x_new[0:nx*ny], deltax[0:nx*ny], Ap[0:nx*ny], p[0:nx*ny], r[0:nx*ny], b[0:nx*ny], v[0:nx*ny], Fx[0:nx*ny], Fxold[0:nx*ny], xold[0:nx*ny], bndN[0:nx], bndE[0:ny], bndS[0:nx], bndW[0:ny], options)
+    {
+		// start timer
+		timespent = -omp_get_wtime();
+
+		// main timeloop
+		double tolerance = 1.e-6;
+		int timestep;
+
+		for (timestep = 1; timestep <= nt; timestep++)
+		{
+		    // set x_new and x_old to be the solution
+		    ss_copy(x_old, x_new, N);
+
+		    double residual;
+		    int    converged = 0;
+		    int    it = 1;
+		    for ( ; it <= 50; it++)
+		    {
+		        // compute residual : requires both x_new and x_old
+		        diffusion(x_new, b);
+		        residual = ss_norm2(b, N);
+
+		        // check for convergence
+		        if (residual < tolerance)
+		        {
+		            converged = 1;
+		            break;
+		        }
+
+		        // solve linear system to get -deltax
+		        ss_cg(deltax, b, 200, tolerance, &cg_converged);
+
+		        // check that the CG solver converged
+		        if (!cg_converged) break;
+
+		        // update solution
+		        ss_axpy(x_new, -1.0, deltax, N);
+		    }
+		    iters_newton += it;
+
+		    // output some statistics
+		    //if (converged && verbose_output)
+		    if (converged && verbose_output)
+		        printf("step %d required %d iterations for residual %E\n", timestep, it, residual);
+		    if (!converged)
+		    {
+		        fprintf(stderr, "step %d ERROR : nonlinear iterations failed to converge\n", timestep);
+		        break;
+		    }
+		}
+    }
+
+    // get times
+    timespent += omp_get_wtime();
+    unsigned long long flops_total = flops_diff + flops_blas1;
+
+    ////////////////////////////////////////////////////////////////////
+    // write final solution to BOV file for visualization
+    ////////////////////////////////////////////////////////////////////
+
+    // binary data
+    {
+        FILE* output = fopen("output.bin", "w");
+        fwrite(x_new, sizeof(double), nx * ny, output);
+        fclose(output);
+    }
+
+    // metadata
+    {
+        FILE* output = fopen("output.bov", "wb");
+        fprintf(output, "TIME: 0.0\n");
+        fprintf(output, "DATA_FILE: output.bin\n");
+        fprintf(output, "DATA_SIZE: %d, %d, 1\n", nx, ny);
+        fprintf(output, "DATA_FORMAT: DOUBLE\n");
+        fprintf(output, "VARIABLE: phi\n");
+        fprintf(output, "DATA_ENDIAN: LITTLE\n");
+        fprintf(output, "CENTERING: nodal\n");
+        //fprintf(output, "BYTE_OFFSET: 4\n");
+        fprintf(output, "BRICK_SIZE: 1.0 %f 1.0\n", (ny - 1) * options.dx);
+        fclose(output);
+    }
+
+    // print table sumarizing results
+    printf("--------------------------------------------------------------------------------\n");
+    printf("simulation took %f seconds (%f GFLOP/s)\n", timespent, flops_total / 1e9 / timespent);
+    printf("%u conjugate gradient iterations\n", iters_cg);
+    printf("%u newton iterations\n", iters_newton);
+    printf("--------------------------------------------------------------------------------\n");
+
+    // deallocate global fields
+    free (x_new);
+    free (x_old);
+    free (bndN);
+    free (bndS);
+    free (bndE);
+    free (bndW);
+
+    printf("Goodbye!\n");
+
+    return 0;
+}
+
---
+++ openacc/c_solution/stats.h
@@ -0,0 +1,9 @@
+#ifndef STATS_H
+#define STATS_H
+
+extern unsigned long long flops_diff, flops_bc, flops_blas1;
+extern unsigned int iters_cg, iters_newton;
+extern int verbose_output;
+
+#endif // STATS_H
+
