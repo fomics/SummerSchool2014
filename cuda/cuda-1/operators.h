62ca06ae88addcaee8248a185d54d84607d9ed68
Dmitry Mikushin <dmitry@kernelgen.org>
2014-07-08 12:44:47 +0200

    Adding CUDA exercise 1

---
+++ cuda/cuda-1/operators.h
@@ -0,0 +1,15 @@
+//******************************************
+// operators.f90
+// based on min-app code written by Oliver Fuhrer, MeteoSwiss
+// modified by Ben Cumming, CSCS
+// *****************************************
+
+// Description: Contains simple operators which can be used on 3d-meshes
+
+#ifndef OPERATORS_H
+#define OPERATORS_H
+
+void diffusion(const double* up, double* sp);
+
+#endif // OPERATORS_H
+
